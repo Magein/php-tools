b96102f5f09948556a6b9cbd1263c87be32528e7
xiaomage <1154699897@qq.com>
2019-05-15 18:10:31 +0800

    update

---
+++ src/think/ApiException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\common\extra;
+
+
+use magein\php_tools\think\ApiReturn;
+use think\exception\Handle;
+use think\exception\HttpException;
+
+class ApiException extends Handle
+{
+    public function render(\Exception $e)
+    {
+        // http请求错误
+        if ($e instanceof HttpException) {
+            return ApiReturn::instance()->create($e->getStatusCode(), $e->getMessage(), $e->getHeaders());
+        }
+    }
+}
