151013e03c0801cc1c02bf0c4b75ebf7205a0d25
xiaomage <1154699897@qq.com>
2019-05-27 15:19:44 +0800

    update

--- src/extra/Upload.php
+++ src/extra/Upload.php
@@ -16,43 +16,12 @@ class Upload
      * 保存路径
      * @var string
      */
-    private $savePath = '/uploads/';
+    private $savePath = './uploads/';
 
     /**
      * @var string
      */
     private $store = 'local';
-
-
-    /**
-     * 兼用web接口访问
-     *
-     * 使用场景：为小程序，app提供接口的时候，图片如果是基于项目目录下回导致访问失败
-     *
-     * @var bool
-     */
-    private $useWebUrl = true;
-
-    /**
-     * @var string
-     */
-    private $host = '';
-
-    /**
-     * @param $host
-     */
-    public function setHost($host)
-    {
-        $this->host = $host;
-    }
-
-    /**
-     * @param bool $use
-     */
-    public function setUseWebUrl(bool $use = true)
-    {
-        $this->useWebUrl = $use ? true : false;
-    }
 
     /**
      * @param string $savePath
@@ -77,16 +46,13 @@ class Upload
     }
 
     /**
-     * 上传图片
-     * $file 为空的时候，自动接收field作为接收文件的名称
-     * $file 为字符串的时候，作为接收文件的名称
-     * $file 为File对象的时候，直接使用
-     * @param null|string|File $file
-     * @param int $size 单位是kb
-     * @param array $ext
+     * 文件上传，
+     * @param null $file 支持传递一个file对象，或者字符串，
+     * @param int $size 最大字节数量 默认1M
+     * @param array|string $ext
      * @return array|bool
      */
-    public function image($file = null, $size = 1024, $ext = ['jpeg', 'png', 'gif', 'jpg'])
+    public function file($file = null, $size = 1048576, $ext = [])
     {
         if ($file === null) {
             $field = Request::instance()->param('field', 'file');
@@ -102,12 +68,17 @@ class Upload
             return false;
         }
 
-        $info = $file->validate(
-            [
-                'size' => $size * 1024,
-                'ext' => $ext,
-            ]
-        )->move('./' . $this->savePath);
+        if ($ext && is_string($ext)) {
+            $ext = explode('|', $ext);
+        }
+
+        $rule['size'] = $size;
+
+        if ($ext) {
+            $rule['ext'] = $ext;
+        }
+
+        $info = $file->validate($rule)->move($this->savePath);
 
         /**
          * 这里要区别于保存路径和展示路径
@@ -119,7 +90,7 @@ class Upload
 
         if ($info) {
             return [
-                'url' => $this->getUrl($info->getSaveName()),
+                'url' => trim($this->savePath . $info->getSaveName(), '.'),
                 'save_name' => $info->getSaveName(),
                 'file_name' => $info->getFilename(),
             ];
@@ -131,34 +102,13 @@ class Upload
     }
 
     /**
-     * 获取访问图片链接
-     * @param $name
-     * @return string
+     * 上传图片
+     * @param null $file
+     * @param int $size
+     * @return bool
      */
-    private function getUrl($name)
+    public function image($file = null, $size = 17240)
     {
-        $url = $this->savePath . $name;
-        if ($this->store != 'local') {
-            return $url;
-        }
-
-        if ($this->useWebUrl) {
-
-            if (empty($this->host)) {
-                $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
-            } else {
-                $host = $this->host;
-            }
-
-            $host = trim($host, '/');
-
-            if (!preg_match('/^http/', $host)) {
-                $host = 'http://' . $host;
-            }
-
-            $url = $host . '/' . $url;
-        }
-
-        return $url;
+        return $this->file($file, $size, ['jpg', 'gif', 'gif', 'jpeg']);
     }
 }
