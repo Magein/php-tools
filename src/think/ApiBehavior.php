c26050b956f3fd18e58e15ad58ea0a9214195f33
xiaomage <1154699897@qq.com>
2019-07-05 10:19:52 +0800

    update

---
+++ src/think/ApiBehavior.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace magein\php_tools\think;
+
+use magein\php_tools\traits\Instance;
+use think\Exception;
+use think\exception\HttpException;
+use think\Request;
+use think\Session;
+
+/**
+ * 接口行为验证
+ * Class ApiBehavior
+ * @package app\common
+ */
+class ApiBehavior
+{
+
+    use Instance;
+
+    /**
+     * @param $request
+     * @return bool
+     */
+    public function run(&$request)
+    {
+        return ApiBehavior::instance()->login($request);
+    }
+
+    /**
+     * 用于检测那些是跳过登录验证数据
+     * @param Request|null $request 这个参数是为了兼容手动修改了请求信息中的值，可以不传递
+     * @param $config
+     * @return bool
+     */
+    public function check(Request $request = null, $config = '')
+    {
+        if (empty($config)) {
+            $config = 'api_request_skip_login_check';
+        }
+
+        if (is_string($config)) {
+            $config = config($config);
+        }
+
+        if (empty($config)) {
+            return false;
+        }
+
+        if ($request) {
+            $controller = $request->controller();
+            $action = $request->action(true);
+        } else {
+            $controller = Request::instance()->controller();
+            $action = Request::instance()->action(true);
+        }
+
+        $check = function ($controller, $action = '*') use ($config) {
+
+            $path = $controller . '/' . $action;
+
+            if (in_array($path, $config)) {
+                return true;
+            }
+
+            return false;
+        };
+
+        if ($check($controller)) {
+            return true;
+        }
+
+        if ($check($controller, $action)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 验证票据
+     * @param Request|null $request
+     * @param string $sign
+     */
+    public function ticket(Request $request = null, $sign = 'ticket')
+    {
+        if (empty($request)) {
+            $request = Request::instance();
+        }
+
+        $ticket = $request->header($sign);
+
+        if (empty($ticket)) {
+            throw new HttpException(1001, '无效的请求信息');
+        }
+
+        try {
+            Session::init(['id' => $ticket]);
+        } catch (Exception $exception) {
+            throw new HttpException(1000, '服务器内部出错误');
+        }
+
+        if ($ticket !== Session::get('session_ticket')) {
+            throw new HttpException(1002, '无效的请求信息');
+        }
+    }
+
+    /**
+     * 用户登录
+     * @param Request $request
+     * @param ApiLogin|null $login
+     * @return bool
+     */
+    public function login(Request $request, ApiLogin $login = null)
+    {
+        if ($this->check($request)) {
+            return true;
+        }
+
+        $this->ticket($request);
+
+        $id = $login->id();
+
+        if (empty($id)) {
+            throw new HttpException(1010, '请先登录');
+        }
+
+        return true;
+    }
+}
---
+++ src/think/ApiLogin.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace magein\php_tools\think;
+
+
+use magein\php_tools\common\RandString;
+use think\Exception;
+use think\exception\HttpException;
+use think\Session;
+use magein\php_tools\traits\Instance;
+
+/**
+ * api登录
+ * Class ApiLogin
+ * @package magein\php_tools\think
+ */
+class ApiLogin
+{
+    use Instance;
+
+    /**
+     * 登录数据
+     */
+    const USER_AUTH = 'user_auth_data';
+
+    /**
+     * 加密的登录数据
+     */
+    const USER_AUTH_SIGN = 'user_auth_sign';
+
+    /**
+     * 用户登录的有效期
+     */
+    const USER_LOGIN_EXPIRE_TIME = 'user_login_expire_time';
+
+    /**
+     * @param null $data
+     * @param int $expire_time
+     * @return null
+     */
+    public function set(&$data = null, $expire_time = 172800)
+    {
+        if (empty($data)) {
+            Session::set(self::USER_AUTH, null);
+            return true;
+        }
+
+        if (isset($data['ticket']) && $data['ticket']) {
+            $ticket = $data['ticket'];
+        } else {
+            // 生成ticket
+            if (isset($data['id']) && $data['id']) {
+                $ticket = $data['id'] . RandString::instance()->make(48);
+            } else {
+                $ticket = RandString::instance()->make(16) . md5('Y-m-d h:i:s');
+            }
+        }
+
+        $ticket = sha1(md5($ticket));
+
+        try {
+            Session::init(['id' => $ticket]);
+        } catch (Exception $exception) {
+            throw new HttpException(1000, '服务器内部出错误');
+        }
+
+        // 用户登录数据
+        Session::set(self::USER_AUTH, $data);
+        Session::set('session_ticket', $ticket);
+
+        if ($data) {
+            // 用户登录标识
+            Session::set(self::USER_AUTH_SIGN, $data ? dataAuthSign($data) : null);
+            // 用户登录有效时间
+            Session::set(self::USER_LOGIN_EXPIRE_TIME, time() + $expire_time);
+        }
+
+        $data['ticket'] = $ticket;
+
+        return $data;
+    }
+
+    /**
+     * @param null $key
+     * @return bool|mixed|string
+     */
+    public function get($key = null)
+    {
+        $user_auth = Session::get(self::USER_AUTH);
+        $user_auth_sign = Session::get(self::USER_AUTH_SIGN);
+
+        if (empty($user_auth) || empty($user_auth_sign) || dataAuthSign($user_auth) !== $user_auth_sign) {
+            return false;
+        }
+
+        $expire_time = Session::get(self::USER_LOGIN_EXPIRE_TIME);
+
+        // 登录时间已经过期
+        if ($expire_time < time()) {
+            $this->set();
+            return false;
+        }
+
+        if ($key) {
+            return isset($user_auth[$key]) ? $user_auth[$key] : '';
+        }
+
+        return $user_auth;
+    }
+
+    /**
+     * 获取登录用户的id
+     * @return bool|mixed|string
+     */
+    public function id()
+    {
+        return $this->get('id');
+    }
+}
