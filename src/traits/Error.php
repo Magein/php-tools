c156bfc79b68edd0a2d02c98bdc4dc9fc2f210ce
xiaomage <1154699897@qq.com>
2019-11-28 17:31:35 +0800

    update

--- src/traits/Error.php
+++ src/traits/Error.php
@@ -43,7 +43,6 @@ trait Error
             if (method_exists($this->error, 'getError')) {
                 return call_user_func_array([$this->error, 'getError'], []);
             }
-
         }
 
         return $this->error ?: '';
@@ -51,11 +50,13 @@ trait Error
 
     /**
      * @param string $error
+     * @param integer $code
      * @return bool
      */
-    public function setError($error = '出错啦~~')
+    public function setError($error = '出错啦~~', $code = 0)
     {
         $this->error = $error;
+        $this->code = $code;
 
         // 如果code为1 则修改为0，自定义code则不进行修改
         if ($this->code == 1) {
